a38b2f173014e4c4ac094904d2816c338ffdd1ee
Gorohov Valerij <amadeys150993@yandex.ru>
2017-02-06 15:35:25 +0300

    вернул миграцию

---
+++ runmeter-server/console/migrations/m170131_104225_create_admin_usere.php.php
@@ -0,0 +1,17 @@
+<?php
+
+use yii\db\Migration;
+
+class m170118_083842_add_backend_fields_in_user_table extends Migration
+{
+
+	// Use safeUp/safeDown to run migration code within a transaction
+	public function safeUp()
+	{
+	}
+
+	public function safeDown()
+	{
+	}
+
+}
